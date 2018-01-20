e066fcd84a73d717e496d9906033f023fd5e7348
julianbei <julian@contentpass.de>
2018-01-20 19:03:48 +0100

    Simplify loops

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 var ws281x = require('rpi-ws281x-native');
 
-var NUM_LEDS = 10,
+var NUM_LEDS = 25,
         pixelData = new Uint32Array(NUM_LEDS);
 
 ws281x.init(NUM_LEDS);
@@ -12,17 +12,8 @@ process.on('SIGINT', function () {
 });
 
 for(var i = 0; i < NUM_LEDS; i++) {
-    pixelData[i] = 0xffcc22;
+    pixelData[i] = 0xffffff;
 }
 ws281x.render(pixelData);
-
-// ---- animation-loop
-var t0 = Date.now();
-setInterval(function () {
-    var dt = Date.now() - t0;
-
-    ws281x.setBrightness(
-        Math.floor(Math.sin(dt/1000) * 128 + 128));
-}, 1000 / 30);
 
 console.log('Press <ctrl>+C to exit.');
