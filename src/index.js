16d57321d4552f77b1ae1d8f98709a3b017c241b
julianbei <julian@contentpass.de>
2018-01-20 19:15:46 +0100

    Slow Color Change

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 var ws281x = require('rpi-ws281x-native');
 
-var NUM_LEDS = 60,
+var NUM_LEDS = 10,
         pixelData = new Uint32Array(NUM_LEDS);
-
-ws281x.init(NUM_LEDS);
 
 ws281x.init(NUM_LEDS);
 
@@ -13,29 +11,18 @@ process.on('SIGINT', function () {
   process.nextTick(function () { process.exit(0); });
 });
 
+function rgb2Int(r, g, b) {
+  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+}
 
 // ---- animation-loop
 var offset = 0;
 setInterval(function () {
   for (var i = 0; i < NUM_LEDS; i++) {
-    pixelData[i] = colorwheel((offset + i) % 256);
+    pixelData[i] = rgb2Int(offset, offset, offset);
   }
-
-  offset = (offset + 1) % 256;
+  offset += 10;
   ws281x.render(pixelData);
-}, 1000 / 30);
+}, 1000);
 
 console.log('Press <ctrl>+C to exit.');
-
-
-// rainbow-colors, taken from http://goo.gl/Cs3H0v
-function colorwheel(pos) {
-  pos = 255 - pos;
-  if (pos < 85) { return rgb2Int(255 - pos * 3, 0, pos * 3); }
-  else if (pos < 170) { pos -= 85; return rgb2Int(0, pos * 3, 255 - pos * 3); }
-  else { pos -= 170; return rgb2Int(pos * 3, 255 - pos * 3, 0); }
-}
-
-function rgb2Int(r, g, b) {
-  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
-}
